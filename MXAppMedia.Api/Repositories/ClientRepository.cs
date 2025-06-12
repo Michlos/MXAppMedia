5bc354fe72218db3a67a78be5217d9993abdb082
Michlos <michlos@gmail.com>
2025-06-12 20:53:52 -0300

    feat: add ClientRepository and IClientRepository

---
+++ MXAppMedia.Api/Repositories/ClientRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MXAppMedia.Api.Context;
+using MXAppMedia.Api.Models;
+
+namespace MXAppMedia.Api.Repositories;
+
+public class ClientRepository : IClientRepository
+{
+    private readonly AppDbContext _context;
+    public ClientRepository(AppDbContext context)
+    {
+        _context = context;
+        
+    }
+
+    public async Task<Client> AddClientAsync(Client client)
+    {
+        await _context.Clients.AddAsync(client);
+        await _context.SaveChangesAsync();
+        return client;
+    }
+
+    public async Task<Client> DeleteClientAsync(Guid id)
+    {
+        var client = _context.Clients.Find(id);
+        if (client == null) return null;
+        _context.Clients.Remove(client);
+        await _context.SaveChangesAsync();
+        return client;
+    }
+
+    public async Task<IEnumerable<Client>> GetAllClientsAsync()
+    {
+        return await _context.Clients.ToListAsync();
+    }
+
+    public async Task<Client?> GetClientByIdAsync(int id)
+    {
+        return await _context.Clients
+            .Where(c => c.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<Client> UpdateClientAsync(Client client)
+    {
+        _context.Entry(client).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return client;
+    }
+
+}
---
+++ MXAppMedia.Api/Repositories/IClientRepository.cs
@@ -0,0 +1,35 @@
+﻿using MXAppMedia.Api.Models;
+
+namespace MXAppMedia.Api.Repositories;
+
+public interface IClientRepository
+{
+    /// <summary>
+    /// Retrieves a client by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the client.</param>
+    /// <returns>The client with the specified ID, or null if not found.</returns>
+    Task<Client?> GetClientByIdAsync(int id);
+    /// <summary>
+    /// Retrieves all clients.
+    /// </summary>
+    /// <returns>A list of all clients.</returns>
+    Task<IEnumerable<Client>> GetAllClientsAsync();
+    /// <summary>
+    /// Adds a new client.
+    /// </summary>
+    /// <param name="client">The client to add.</param>
+    /// <returns>The added client.</returns>
+    Task<Client> AddClientAsync(Client client);
+    /// <summary>
+    /// Updates an existing client.
+    /// </summary>
+    /// <param name="client">The client to update.</param>
+    /// <returns>The updated client.</returns>
+    Task<Client> UpdateClientAsync(Client client);
+    /// <summary>
+    /// Deletes a client by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the client to delete.</param>
+    Task<Client> DeleteClientAsync(Guid id);
+}
