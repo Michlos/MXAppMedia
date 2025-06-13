19dd1f387b023e379c00d81f43d915870cbde3ff
Michlos <michlos@gmail.com>
2025-06-12 21:29:37 -0300

    feat: add ClientServices and your interface

---
+++ MXAppMedia.Api/Services/ClientService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+
+using MXAppMedia.Api.DTOs;
+using MXAppMedia.Api.Repositories;
+
+namespace MXAppMedia.Api.Services;
+
+public class ClientService : IClientService
+{
+    private readonly IClientRepository _clientRepository;
+    private readonly IMapper _mapper;
+
+    public ClientService(IClientRepository clientRepository, IMapper mapper)
+    {
+        _clientRepository = clientRepository;
+        _mapper = mapper;
+    }
+
+    public async Task AddClient(ClientDTO clientDto)
+    {
+        var clientEntity = _mapper.Map<Models.Client>(clientDto);
+        await _clientRepository.AddClientAsync(clientEntity);
+        clientDto.Id = clientEntity.Id;
+
+    }
+
+    public async Task DeleteClient(int id)
+    {
+        var clientEntity = _clientRepository.GetClientByIdAsync(id);
+        await _clientRepository.DeleteClientAsync(clientEntity.Id);
+    }
+
+    public async Task<IEnumerable<ClientDTO>> GetAllClients()
+    {
+        var clientEntity = await _clientRepository.GetAllClientsAsync();
+        return _mapper.Map<IEnumerable<ClientDTO>>(clientEntity);
+    }
+
+    public Task<ClientDTO> GetClientById(int id)
+    {
+        var clientEntity = _clientRepository.GetClientByIdAsync(id);
+        return _mapper.Map<Task<ClientDTO>>(clientEntity);
+    }
+
+    public async Task UpdateClient(ClientDTO clientDto)
+    {
+        var clientEntity = _mapper.Map<Models.Client>(clientDto);
+        await _clientRepository.UpdateClientAsync(clientEntity);
+    }
+}
---
+++ MXAppMedia.Api/Services/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using MXAppMedia.Api.DTOs;
+
+namespace MXAppMedia.Api.Services;
+
+public interface IClientService
+{
+
+    Task<ClientDTO> GetClientById(int id);
+    Task<IEnumerable<ClientDTO>> GetAllClients();
+    Task AddClient(ClientDTO clientDto);
+    Task UpdateClient(ClientDTO clientDto);
+    Task DeleteClient(int id);
+
+}
