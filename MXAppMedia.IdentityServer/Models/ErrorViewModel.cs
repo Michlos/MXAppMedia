623f138aabb1241f4880041f61b63ab7d5f9593b
Michlos <michlos@gmail.com>
2025-07-12 09:28:47 -0300

    fix: fixed button CreateMedia in client list view.

---
+++ MXAppMedia.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MXAppMedia.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
